7be0d465b018bc4e97a4b3b66342971e5d5f5134
stefancho <stefanstoqnov@gmail.com>
2023-02-15 21:18:28 +0200

    Time exercise

---
+++ clock/Clock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time c1 = new Time();
+            c1.SetTime(3, 20);
+            c1.AddMinutes(1);
+            Console.WriteLine(c1.ToString());
+
+            Time c2 = new Time();
+            c2.SetTime(3, 22);
+
+            if (c1.Equals(c2)) 
+            {
+                Console.WriteLine("success !");
+            }
+
+
+
+        }
+    }
+}
---
+++ clock/Clock/Time.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clock
+{
+    class Time
+    {
+        //TODO: дефинирайте член-променливите, които пазят информацията за времето
+
+
+        /// <summary>
+        /// Задава текущото време с определени час/минути
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="min"></param>
+        public void SetTime(int h, int min)
+        { 
+            
+        }
+
+        /// <summary>
+        /// Връща компонента час от текущото време
+        /// </summary>
+        /// <returns></returns>
+        public int GetHours()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Връща минуите от текущото време
+        /// </summary>
+        /// <returns></returns>
+        public int GetMinutes()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Връща секундите на текущото време
+        /// </summary>
+        /// <returns></returns>
+        public int GetSeconds()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Добавя определени секунди към текущото време
+        /// </summary>
+        /// <param name="seconds">стойност на добавяните секунди</param>
+        public void AddSeconds(int seconds)
+        { 
+            
+        }
+
+        /// <summary>
+        /// Добавя определени минути към текущото време
+        /// </summary>
+        /// <param name="minutes"></param>
+        public void AddMinutes(int minutes)
+        { 
+        
+        }
+
+        /// <summary>
+        /// Добавя часове към текущото време
+        /// </summary>
+        /// <param name="hours"></param>
+        public void AddHours(int hours)
+        { 
+            
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
